30f67d9eaebc0732b05cca97d0adacec05997220
y-u-y-a <yuya_yamada00@yahoo.co.jp>
2019-09-29 13:40:09 +0900

    修正

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: "history",
+  base: "/profile",
   routes: [
     {
         path     : "/",
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    publicPath: "./profile",
+    publicPath: "/profile/",
     outputDir: "docs",
 }
